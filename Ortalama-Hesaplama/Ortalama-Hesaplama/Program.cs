4286eb1844182936c3927a92cabd4d8445507156
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-04-04 12:53:52 +0300

    project update.

--- Ortalama-Hesaplama/Ortalama-Hesaplama/Program.cs
+++ Ortalama-Hesaplama/Ortalama-Hesaplama/Program.cs
@@ -1,28 +1,79 @@
-﻿//Ortalama Hesaplama
-/* Kulanıcıdan alınan derinliğe göre fibonacci serisindeki rakamların
-ortalamasını alıp ekrana yazdıran uygulamayı yazınız.
-Dikkat Edilmesi Gereken Noktalar :
->Kod tekrarından kaçınılmalı.
->Single Responsibility(Bir Class'ın Tek Bir Sorumluluğu Olmalıdır.) kuralına uygun şekilde,
- uygulama sınıflara ve metotlara bölünmeli.
-*/
-Console.Write("Fibonacci dizisi uzunluğu girin : ");
-int Length = int.Parse(Console.ReadLine());
-FibonacciGenerator fibonacciGenerator = new FibonacciGenerator(Length);
-fibonacciGenerator.Generator();
-Console.ReadLine();
-
-
-
-
-public  class FibonacciGenerator
+﻿#region Program.cs
+/*
+ * 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377
+ */
+try
 {
-    public int Length { get; set; }
-    public FibonacciGenerator() {}
-    public FibonacciGenerator(int Length) { this.Length = Length; }
-    
-    public void Generator()
+    Console.Write("Fibonacci dizisi uzunluğu girin : ");
+    long length = long.Parse(Console.ReadLine());
+    Fibonacci fibonacci = FibonacciGenerator.Generator(length);
+    FibonacciAverage fibonacciAverage = new FibonacciAverage();
+    try
     {
-
+        Console.WriteLine(fibonacciAverage.GetAverage(fibonacci.fibonacciNumbers));
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
     }
 }
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+#endregion
+
+#region Fibonacci.cs
+public class Fibonacci
+{
+    public Fibonacci() { }
+    public long[] fibonacciNumbers { get;  set; }
+
+}
+#endregion
+
+# region FibonacciGenerator.cs
+public class FibonacciGenerator
+{
+    public static Fibonacci Generator(long length)
+    {
+        Fibonacci fibonacci = new Fibonacci();
+        long[] numbers = new long[length];
+        long x = 0;
+        for (long i = 0; i < length; i++)
+        {
+            if (i < 2)
+            {
+                numbers[i] = i;
+                x += i;
+            }
+            else
+            {
+                long temp = 0;
+
+
+                numbers[i] = temp;
+            }
+        }
+
+
+        fibonacci.fibonacciNumbers = numbers;
+        return fibonacci;
+    }
+}
+#endregion
+
+#region FibonacciAverage
+public class FibonacciAverage
+{
+    public long GetAverage(long[] FibonacciArray)
+    {
+        long avarage = 0;
+        foreach (var item in FibonacciArray)
+        {
+            avarage += item;
+        }
+        return avarage;
+    }
+}
+#endregion
