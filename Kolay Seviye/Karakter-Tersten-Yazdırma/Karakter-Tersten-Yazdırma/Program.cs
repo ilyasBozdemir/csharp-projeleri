90d4372beeca522f205c03a797ca518c7a99310a
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-04-04 14:49:00 +0300

    project added.

--- Kolay Seviye/Karakter-Tersten-Yazdırma/Karakter-Tersten-Yazdırma/Program.cs
+++ Kolay Seviye/Karakter-Tersten-Yazdırma/Karakter-Tersten-Yazdırma/Program.cs
@@ -5,3 +5,10 @@ bir önceki karakter ile yer değiştiren console uygulamasını yazınız.
 Örnek: Input: Merhaba Hello Question
 Output: erhabaM elloH uestionQ 
  */
+
+Console.Write("input: ");
+string veri = Console.ReadLine();
+var dizi = veri.ToArray();
+var yeniDizi = dizi.Reverse();
+string yeniVeri = string.Join("", yeniDizi);
+Console.WriteLine($"Output: {yeniVeri}");
