3fb64f790efea4a7f0e586cee209c9b5b4d7c426
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-04-05 00:09:00 +0300

    project add.

--- Orta Seviye/Mutlak-Kare-Alma/Mutlak-Kare-Alma/Program.cs
+++ Orta Seviye/Mutlak-Kare-Alma/Mutlak-Kare-Alma/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿var data = Console.ReadLine();
+var arrayData = data.Split(" ");
+const int deger = 67;
+int resultK = 0, resultB = 0;
+for (int i = 0; i < arrayData.Length; i++)
+{
+    var sayi = int.Parse(arrayData[i]);
+    if (sayi < deger)
+        resultK += (deger - sayi);
+    else if (sayi > deger)
+    {
+        var x = (double)Math.Abs(deger - sayi);
+        resultB += (int)Math.Pow(x, 2);
+    }
+}
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.Write(resultK + " " + resultB);
+Console.ForegroundColor = ConsoleColor.White;
