7af4bc33367e7ca7b759222a009772951a82cd98
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-04-04 13:57:00 +0300

    project created.

---
+++ Üçgen-Olusturma/Üçgen-Olusturma/Program.cs
@@ -0,0 +1,32 @@
+﻿#region Program.cs
+using System.Text;
+
+try
+{
+    Console.Write("Üçgen uzunluğunu girin : ");
+    int length = int.Parse(Console.ReadLine());
+    Triangle triangle = Triangle.Create(length);
+    
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+#endregion Triangle.cs
+public class Triangle
+{
+    StringBuilder stringLine;
+    public static Triangle Create(int length)
+    {
+
+        return new Triangle();
+    }
+}
+#region s
+
+#endregion
+
+#region s
+
+#endregion
