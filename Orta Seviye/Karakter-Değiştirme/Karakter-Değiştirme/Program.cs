10fa4645c86192bc6fceaff014f599e9ac65002f
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-04-05 00:44:47 +0300

    project added.

--- Orta Seviye/Karakter-Değiştirme/Karakter-Değiştirme/Program.cs
+++ Orta Seviye/Karakter-Değiştirme/Karakter-Değiştirme/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿/*# Algoritma
+Verilen string ifade içerisindeki ilk ve son karakterin yerini değiştirip
+tekrar ekrana yazdıran console uygulamasını yazınız.
+Input: Merhaba Hello Algoritma x
+Output: aerhabM oellH algoritmA x
+*/
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write("Input : ");
+string data = Console.ReadLine();
+string[] dataArray = data.Split(" ");
+char temp = ' ', temp2 = ' ';
+for (int i = 0; i < dataArray.Length; i++)
+{
+    data = dataArray[i];
+    char[] charArray = data.ToArray();
+    var lastIndex = charArray.Length -1 ;
+    temp = charArray[0];
+    temp2 = charArray[lastIndex];
+    charArray[lastIndex] = temp;
+    charArray[0] = temp2;
+    var newData = string.Join("", charArray);
+    Console.ForegroundColor = ConsoleColor.Cyan;
+    Console.Write(newData + " ");
+    Console.ForegroundColor = ConsoleColor.White;
+}
