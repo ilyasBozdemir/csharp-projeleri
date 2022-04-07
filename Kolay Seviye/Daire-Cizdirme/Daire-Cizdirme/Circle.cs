fe54903258267faedc474cbdba222d83a550b0f0
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-04-07 12:33:42 +0300

    project added.

---
+++ Kolay Seviye/Daire-Cizdirme/Daire-Cizdirme/Circle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Daire_Cizdirme
+{
+    public class Circle
+    {
+        private double radius;
+        public double Radius
+        {
+            get { return radius; }
+            set{ radius = value; }
+        }
+        private double thickness;
+        public double Thickness{ get { return thickness; } set { thickness = value; } }
+        public Circle(double radius, double thickness = 0.4) 
+        {
+            this.Thickness = thickness;
+            this.Radius = radius;
+        }
+        public Circle ()  { }//yapıcı method
+        ~Circle() {}//yıkıcı method
+    }
+    public class DrawCircle
+    {
+        public StringBuilder GetDraw(Circle circle)
+        {
+            double rIn = circle.Radius - circle.Thickness,//iç çap
+                   rOut = circle.Radius + circle.Thickness;//dış çap
+            StringBuilder sb = new StringBuilder ();
+
+            for (double y = circle.Radius; y >= -circle.Radius; --y)// 0,5' -0,5'
+            {
+                for (double x = -circle.Radius; x < rOut; x += 0.5)
+                {
+                    double value = x * x + y * y;// = ( x^2 + y^2 )
+                    if (value >= rIn * rIn && value <= rOut * rOut)// ( x^2 + y^2 ) iç çap ve dış çapta ise
+                    {
+                        sb.Append('*');// karakteri bas
+                    }
+                    else// (x ^ 2 + y ^ 2) iç çap ve dış çapta değil ise
+                    {
+                        sb.Append(' ');// boşluk bas
+                    }
+                }
+                sb.AppendLine();//yeni satıra geç
+            }
+            return sb;
+        }
+    }
+
+}
--- Kolay Seviye/Daire-Cizdirme/Daire-Cizdirme/Program.cs
+++ Kolay Seviye/Daire-Cizdirme/Daire-Cizdirme/Program.cs
@@ -1,1 +1,24 @@
-﻿double r = 5;
+﻿/* Şekil Çizimi
+Kullanıcıdan alınan yarıçapa göre console'a Daire çizen uygulamayı yazınız.
+Dikkat Edilmesi Gereken Noktalar :
+Kod tekrarından kaçınılmalı
+Single Responsibility kuralına uygun şekilde, uygulama sınıflara ve metotlara bölünmeli.*/
+using Daire_Cizdirme;
+
+Basla:
+double radius = 5.0,/*yarıçap*/ thickness = 0.4;//kalınlık
+Console.Write("Yarıçap girin : ");
+bool state = double.TryParse(Console.ReadLine(), out radius);
+if (!state || radius < 0)
+{
+    Console.Write("pozitif değer girin!\n");
+    goto Basla;
+}
+
+Circle circle = new Circle(radius: radius, thickness: thickness);
+DrawCircle drawCircle = new DrawCircle();
+var circleData = drawCircle.GetDraw(circle);
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("\n"+circleData);
+Console.ForegroundColor = ConsoleColor.Gray;
+Console.ReadLine();
