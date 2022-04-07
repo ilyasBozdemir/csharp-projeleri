ddb251210c7a634ffeb7125034f2c5efdbecdcd0
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-04-07 17:10:05 +0300

    .

--- Orta Seviye/Alan-Hesaplama/Alan-Hesaplama/Program.cs
+++ Orta Seviye/Alan-Hesaplama/Alan-Hesaplama/Program.cs
@@ -1,1 +1,79 @@
-﻿
+﻿using Alan_Hesaplama;
+Shape shape;
+int result = 0;
+veriAl:
+Console.WriteLine("Şekil Seçimi Yapın.");
+string[] sekiller = { "Circle", "Triangle", "Square", "Rectangle", "Cube", "Cone", "Pyramid", "Cylinder" };
+string[] secenekler = { "Çevre", "Alan", "Hacim" };
+Console.ForegroundColor = ConsoleColor.Yellow;
+for (int i = 0; i < sekiller.Length; i++)
+{
+    Console.WriteLine($"{sekiller[i] } için {i + 1} tuşlayın");
+}
+Console.ForegroundColor = ConsoleColor.Gray;
+int.TryParse(Console.ReadLine(), out result);
+Console.Clear();
+Console.WriteLine($"{result} -> {sekiller[result - 1]} seçildi ");
+//switch (result)
+//{
+//    #region case 1: 
+//    case 1:
+//        shape = new Circle();
+//        break;
+//    #endregion
+//    #region case 2: 
+//    case 2:
+//        shape = new Triangle();
+//        break;
+//    #endregion
+//    #region case 3: 
+//    case 3:
+//        shape = new Square();
+//        break;
+//    #endregion
+//    #region case 4: 
+//    case 4:
+//        shape = new Rectangle();
+//        break;
+//    #endregion
+//    #region case 5: 
+//    case 5:
+//        shape = new Cube();
+//        break;
+//    #endregion
+//    #region case 6: 
+//    case 6:
+//        shape = new Cone();
+//        break;
+//    #endregion
+//    #region case 7: 
+//    case 7:
+//        shape = new Pyramid();
+//        break;
+//    #endregion
+//    #region case 8: 
+//    case 8:
+//        shape = new Cylinder();
+//        break;
+//    #endregion
+//    #region case default:
+//    default:
+//        Console.ForegroundColor = ConsoleColor.Red;
+//        Console.Write("Lütfen seçim yapın");
+//        goto veriAl;
+//        #endregion
+//}
+for (int i = 0; i < secenekler.Length; i++)
+{
+    Console.WriteLine($"{secenekler[i] } için {i + 1};");
+}
+int secenek;
+int.TryParse(Console.ReadLine(), out secenek);
+Console.WriteLine($"{secenek} -> {secenekler[secenek - 1]} seçildi.");
+
+object[] data = { result, secenek };
+
+
+
+
+Console.Read();
---
+++ Orta Seviye/Alan-Hesaplama/Alan-Hesaplama/Shape.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alan_Hesaplama
+{
+    //şekil için class yerine struct kullanilabilirdi ama, virtual kullanamadığımoz için class kullandık
+    public abstract class Shape//Şekil
+    {
+        public virtual void GetPerimeterCalculation()//çevre
+        {
+          
+        }
+        public virtual void GetAreaCalculation()//alan
+        {
+           
+        }
+        public virtual void GetVolumeCalculation()//hacim
+        {
+
+        }
+    }
+    public class Circle : Shape /*daire*/
+    { 
+        public Circle()
+        {
+
+        }
+    }
+    public class Triangle : Shape/*üçgen*/
+    {
+        public Triangle()
+        {
+
+        }
+    }
+    public class Square : Shape/*kare*/
+    {
+        public double kenarUzunlugu { get; set; }
+        public Square(double kenarUzunlugu,string secenek)
+        {
+            this.kenarUzunlugu = kenarUzunlugu;
+            switch (secenek)
+            {
+                case "Çevre":
+
+                    break;
+                case "Alan":
+
+                    break;
+                case "Hacim":
+
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    public class Rectangle : Shape/*dikdörtgen*/
+    {
+        public Rectangle()
+        {
+
+        }
+    }
+    public class Cube : Shape/*küp*/
+    {
+        public Cube()
+        {
+
+        }
+    }
+    public class Cone : Shape/*koni*/
+    {
+        public Cone()
+        {
+
+        }
+    }
+    public class Pyramid : Shape/*piramit*/
+    {
+        public Pyramid()
+        {
+
+        }
+    }
+    public class Cylinder : Shape/*silindir*/
+    {
+        public Cylinder()
+        {
+
+        }
+
+    }
+}
