6231c08452047a81a8e5aa0f126df2544f99a877
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-04-20 13:14:23 +0300

    project updated.

---
+++ Orta-Seviye/Alan-Hesaplama/Alan-Hesaplama/Helpers/ShapeHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alan_Hesaplama.Helpers
+{
+    public class ShapeHelper//her şekilin çevre formülü farklıdır bunu yapan sınıfımız.
+    {
+        public static double PerimeterCalculation(Type type)//çevre
+        {
+            
+
+            double cevre = 0;
+
+            return cevre;
+        }
+        public static double AreaCalculation(Type type)//alan
+        {
+            double alan = 0;
+
+            return alan;
+        }
+        public static double VolumeCalculation(Type type)//hacim
+        {
+            double hacim = 0;
+
+            return hacim;
+        }
+        private static void getType()
+        {
+
+        }
+
+    }
+}
---
+++ Orta-Seviye/Sessiz-Harf/Sessiz-Harf/Program.cs
@@ -0,0 +1,28 @@
+﻿string sesliHarfler = "aeıioöuü";
+Console.Write("Metni Girin: ");
+var data = Console.ReadLine();
+var dataArr = data.Split(' ');
+Console.ForegroundColor = ConsoleColor.Yellow;
+for (int i = 0; i < dataArr.Length; i++)
+    Console.Write($"{IkıTaneSessizHarfYanYanaMi(dataArr[i])} ");
+Console.ForegroundColor = ConsoleColor.Gray;
+Console.Read();
+
+bool IkıTaneSessizHarfYanYanaMi(string data)
+{
+    byte sayac = 0;
+    bool state = false;
+    foreach (var item in data.ToLower())
+    {
+        state = sesliHarfler.Contains(item);
+        if (!state)
+        {
+            sayac++;
+            if (sayac == 2)
+                return true;
+        }
+        else
+            sayac = 0;///sesli harf görürsen 0'la
+    }
+    return state;
+}
