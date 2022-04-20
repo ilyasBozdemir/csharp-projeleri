2f4a8265689334d7105a996ab5e0b8ece249b95a
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-04-20 21:18:22 +0300

    project updated.

--- Zor-Seviye/Barcode-Generator-and-Reader/Barcode-Generator-and-Reader/Program.cs
+++ Zor-Seviye/Barcode-Generator-and-Reader/Barcode-Generator-and-Reader/Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿using BarcodeLib;
+using BarcodeLib.BarcodeReader;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Text;
+
+BarcodeCreate("869412244212");
+BarcodeRead();
+
+
+void BarcodeCreate(string NumericString)
+{
+    Barcode barcodLib = new Barcode();
+
+    int imageWidth = 290; 
+    int imageHeight = 120; 
+    Color foreColor = Color.Black; 
+    Color backColor = Color.White; 
+
+    Image barcodeImage = barcodLib.Encode
+        (TYPE.UPCA, NumericString, foreColor, backColor, imageWidth, imageHeight);
+
+    barcodeImage.Save(@"D:\Barcode.png", ImageFormat.Png);
+}
+void BarcodeRead()
+{
+    string[] BarcodeUPCA = BarcodeReader.read(@"D:\Barcode.png",
+        BarcodeReader.UPCA);
+
+    StringBuilder builder = new StringBuilder();
+    foreach (string value in BarcodeUPCA)
+    {
+        builder.Append(value);
+        Console.WriteLine(value);
+    }
+    //Console.WriteLine("UPCA Code:" + builder.ToString());
+}
