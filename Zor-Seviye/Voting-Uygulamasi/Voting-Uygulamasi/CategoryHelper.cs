b702689daa0e07c2ed98f15d76b3d9c1f3e134ad
ilyasBozdemir <Bozdemir.ib70@gmail.com>
2022-04-21 11:06:04 +0300

    project finished.

--- Zor-Seviye/Voting-Uygulamasi/Voting-Uygulamasi/CategoryHelper.cs
+++ Zor-Seviye/Voting-Uygulamasi/Voting-Uygulamasi/CategoryHelper.cs
@@ -9,14 +9,14 @@ namespace Voting_Uygulamasi
     public class CategoryHelper
     {
         private Categories _categories;
-
         public CategoryHelper(Categories categories)
         {
             this._categories = categories;
         }
 
-        public void WriteConsole(Dictionary<string, int> Voting,string categoryName)
+        public void WriteConsole(Dictionary<string, int> Voting, string categoryName)
         {
+basla:
             byte sayac = 0, secim = 0;
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine();
@@ -26,14 +26,76 @@ namespace Voting_Uygulamasi
             foreach (var item in Voting)
             {
                 sayac++;
-                Console.WriteLine($"{item.Key} için {sayac} tuşlayın");
+                Console.WriteLine($"\"{item.Key}\" için {sayac} tuşlayın");
                 //categories.FilmVoting[item.Key] = 1;
+            }
+            Console.WriteLine($"Yukarıda ki {categoryName} kategorilerinden birisini seçin: ");
+            secim = byte.Parse(Console.ReadLine());
+            if (secim > 0 && secim <= sayac)
+            {
+                byte i = 0;
+                //Console.WriteLine($"{secim} - {sayac}");
+                foreach (var item in Voting)
+                {
+                    i++;
+                    if (secim == i)
+                    {
+                        Voting[item.Key] += 1;
+                        Console.WriteLine($"Oy Verildi : {item.Key}");
+                    }
+                }
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Lütfen Listeden Seçim Yapın.");
+                goto basla;
+            }
+        }
+        public void GetVotingResult()
+        {
+            Console.WriteLine();
+            Console.WriteLine("---Sonuçlar---");
+            Console.WriteLine();
+            int ToplamOy = toplamOyGetir(_categories.FilmVoting);
+            foreach (var item in _categories.FilmVoting)
+            {
+                string key = item.Key;
+                int value = item.Value;
+                string data = $"{key} için toplam oy sayısı : {value}";
+                double yuzdelik = (value * 100 / ToplamOy);
+                Console.WriteLine(data + $" %{yuzdelik}");
+            }
+            Console.WriteLine();
+            ToplamOy = toplamOyGetir(_categories.SporVoting);
+            foreach (var item in _categories.SporVoting)
+            {
+                string key = item.Key;
+                int value = item.Value;
+                string data = $"{key} için toplam oy sayısı : {value}";
+                double yuzdelik = (value * 100 / ToplamOy);
+                Console.WriteLine(data + $" %{yuzdelik}");
+            }
+            Console.WriteLine();
+            ToplamOy = toplamOyGetir(_categories.TechStackVoting);
+            foreach (var item in _categories.TechStackVoting)
+            {
+                string key = item.Key;
+                int value = item.Value;
+                string data = $"{key} için toplam oy sayısı : {value}";
+                double yuzdelik = (value * 100 / ToplamOy);
+                Console.WriteLine(data + $" %{yuzdelik}");
             }
         }
 
-        public void GetVotingResult()
+        private int toplamOyGetir(Dictionary<string, int> Voting)
         {
-          
+            int sayi = 0;
+            foreach (var item in Voting)
+            {
+                sayi += item.Value;
+            }
+            return sayi;
         }
     }
 }
--- Zor-Seviye/Voting-Uygulamasi/Voting-Uygulamasi/Program.cs
+++ Zor-Seviye/Voting-Uygulamasi/Voting-Uygulamasi/Program.cs
@@ -1,17 +1,24 @@
 ﻿using Voting_Uygulamasi;
+Categories categories = new Categories();
+CategoryHelper categoryHelper = new CategoryHelper(categories);
 Console.WriteLine("Aşağıdaki kategorilere oy vererek anketimize katılabilirsiniz.");
+basla:
 Console.Write("Kullanıcı Adınızı Girin: ");
 User user = new User();
 UserControl userControl = new UserControl();
 user.userName = Console.ReadLine();
 if (userControl.GetUser(user))
 {
-    Categories categories = new Categories();
-    CategoryHelper categoryHelper = new CategoryHelper(categories);
+   
     categoryHelper.WriteConsole(categories.FilmVoting, "film");
     categoryHelper.WriteConsole(categories.SporVoting, "spor");
     categoryHelper.WriteConsole(categories.TechStackVoting, "tech stack");
     categoryHelper.GetVotingResult();
+    Console.Write("Yeniden Başlamak için \"y\" tuşlayın.");
+    if (Console.ReadLine().ToLower() == "y")
+    {
+       goto basla;
+    }
 }
 else
 {
